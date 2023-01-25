2615142eed7eedc08c98a02e7aea165b1c360880
Swarga-codes <sd3526@srmist.edu.in>
2023-01-26 00:12:19 +0530

    added POST requests and input validation

--- vidly/index.js
+++ vidly/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 //middleware
 // app.set("view engine","ejs");
+//middleware
+app.use(express.json());
 const genres = [
     {id:1, name: "Comedy"},
     {id:2, name: "Horror"},
@@ -20,6 +23,23 @@ app.get("/api/genres/:id",(req,res)=>{
 const genre = genres.find(f => f.id === parseInt(req.params.id));
 if(!genre) return res.status(404).send("The requested genre was not found!");
 res.send(genre);
+});
+//POST Request
+app.post("/api/genres/",(req,res) =>{
+    const schema = Joi.object({
+        name:Joi.string().min(5).required(),
+    });
+    const result = schema.validate(req.body);
+    if(result.error){
+        return res.status(400).send(result.error.details[0].message);
+    }
+
+   const genre = {
+    id:genres.length+1,
+    name:req.body.name,
+   };
+   genres.push(genre);
+   res.send(genres);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
