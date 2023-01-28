a30d2486e66c823e3d66b5cc5ee461dc556cdbe0
Swarga-codes <sd3526@srmist.edu.in>
2023-01-28 23:37:56 +0530

    added put request to the genre

--- vidly/index.js
+++ vidly/index.js
@@ -33,13 +33,30 @@ app.post("/api/genres/",(req,res) =>{
     if(result.error){
         return res.status(400).send(result.error.details[0].message);
     }
-
    const genre = {
     id:genres.length+1,
     name:req.body.name,
    };
    genres.push(genre);
    res.send(genres);
+});
+//PUT Request
+app.put("/api/genres/:id",(req,res)=>{
+//whether the id exists or not
+const genre = genres.find(g => g.id === parseInt(req.params.id));
+if(!genre){
+    return res.status(404).send("The requested ID was not found!");
+}
+//validate the given update
+const schema = Joi.object({
+    name:Joi.string().min(5).required()
+});
+const result = schema.validate(req.body);
+if(result.error){
+return res.status(404).send(result.error.details[0].message);
+}
+genre.name=req.body.name;
+res.send(genre);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
