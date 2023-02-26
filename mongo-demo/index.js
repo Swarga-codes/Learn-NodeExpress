f4bd295e9f2da011a0ca7a2bfa9960a5a74829ec
Swarga-codes <sd3526@srmist.edu.in>
2023-02-26 18:01:49 +0530

    learnt how to create and read data from mongodb

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -2,3 +2,35 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
 .then(()=>console.log('Connected to mongo db...'))
 .catch(err=>console.error('Couldnt connect to mongo db',err))
+
+//creating a schema
+const courseSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date:{type:Date,default:Date.now},
+    isPublished:Boolean,
+});
+//compiling the schema into a model
+const Course = mongoose.model('Course',courseSchema);
+//creating a new data
+async function createCourse(){
+const course=new Course({
+    name:'React Development',
+    author: 'Markus',
+    tags: ['frontend','react'],
+    isPublished:true
+});
+const result=await course.save();
+console.log(result);
+}
+// createCourse();
+//reading the data or querying the data
+async function getCourses(){
+    const courses=await Course.find({author:'Markus',isPublished:true})
+    .limit(2)
+    .sort({name:-1})
+    .select({name:1,tags:1});
+    console.log(courses);
+}
+getCourses();
